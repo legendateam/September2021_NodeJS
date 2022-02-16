761d4ac0d9a256e0b69790bcd84783c2440b37bf
legendateam <undertaker176@gmail.com>
2022-02-16 21:57:32 +0200

    cw_1

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+const path = require('path');
+const fs = require('fs');
 // Завдання на практику
 //
 // 1. Спробуйте створити якийсь файл txt, прочитайте з нього дані і одразу, дані які ви отримали запишіть їх в інший файл,
 // в вас вийде невеликий callback hell, пізніше я вам покажу
 // як можна це обійти, але поки зробіть так
 
-const path = require('path');
-const fs = require('fs');
-
 //
 // fs.writeFile(path.join(__dirname, 'classwork_1.txt'), 'classwork', err=>{
 //     if(err) {
 //         console.log(err);
 //     }
-// })
+// });
 //
 // fs.readFile(path.join(__dirname, 'classwork_1.txt'), 'utf8', (err,data) => {
 //     if(err) {
@@ -24,18 +23,18 @@ const fs = require('fs');
 //         }
 //
 //     })
-// })
+// });
 
 // 2. Створіть файл ( можете вручну ) заповніть його якимись даними
 // Прочитайте його, скопіюйте всі дані з нього і перенесіть їх в нову папку та файл в ній,
 // старий файл видаліть після того як все завершиться. Також вийде callback hell
-
+//
 // const writeFile = (callback) => {
 //     fs.writeFile(path.join(__dirname, 'task_2.txt'), 'okten school it`s cool', err => {
 //         if (err) throw err
 //     })
 //     callback()
-// }
+// };
 //
 // const readFile = (callback) => {
 //     fs.readFile(path.join(__dirname, 'task_2.txt'), (err, data) => {
@@ -48,37 +47,37 @@ const fs = require('fs');
 //         })
 //     });
 //     callback()
-// }
+// };
 //
 // const unlink = () => {
 //     fs.unlink(path.join(__dirname, 'task_2.txt'), err => {
 //         if (err) throw err
 //     })
-// }
+// };
 //
 // writeFile(()=> {
 //     readFile(()=> {
 //         unlink()
 //     })
-// })
+// });
 
 //
 // 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані
 // (можуть бути нові папки і файли(в файли запишіть якусь дату) )
 // і напишіть функцію яка буде зчитувати папку і перевіряти якщо дані які в ній лежать - це файли тоді вам потрібно
 // їх очистити, але не видаляти, якщо дані - це папки, вам потрібно їх перейменувати і додати до назви префікс _
-
+//
 // fs.mkdir(path.join(__dirname, 'last_task'), err=> {
 //     if(err) throw err
-// })
-
+// });
+//
 // fs.mkdir(path.join(__dirname,'last_task', 'okten'), { recursive: true} , err=> {
 //     if(err) throw err;
 //     fs.writeFile(path.join(__dirname,'last_task', 'node.txt'), 'module, global,os,path,fs', (err => {
 //         if(err) throw err
 //     }))
-// })
-
+// });
+//
 // const readFolder = () => {
 //     fs.readdir(path.join(__dirname,'last_task'),  (err,data) => {
 //         if(err) throw err;
